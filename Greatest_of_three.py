6c55fe784b2f630c06d4db014a793fc18ead4fd7
ialikh72 <ialikh.w1@gmail.com>
2026-03-15 21:13:13 +0500

    Add project files.

---
+++ Greatest_of_three.py
@@ -0,0 +1,11 @@
+num1=int(input("enter a number1:"))
+num2=int(input("enter a number2:"))
+num3=int(input("enter a number3:"))
+if((num1>num2) and (num1>num3)):
+        print("num1 is greatest")
+elif((num2>num1) and (num2 >num3)):
+        print("num2 is greatest")
+elif((num3>num1) and (num3>num2)):
+        print("num3 is greatest")
+
+
